9a2cb9fa4cec6e8a0fdc3f27bda517213cca39b4
Ben Terry <beterry12@gmail.com>
2020-05-07 15:03:33 -0400

    Updates

---
+++ precache-manifest.0efcc22d61eca442474274995b1069f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "862d14f44e7b4a90efc25096698912d2",
+    "url": "/vantage-ui-2/index.html"
+  },
+  {
+    "revision": "fbd518d96d6c1257e6bd",
+    "url": "/vantage-ui-2/static/css/main.a549f898.chunk.css"
+  },
+  {
+    "revision": "d76825c2c8768f41c502",
+    "url": "/vantage-ui-2/static/js/2.186fb6f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vantage-ui-2/static/js/2.186fb6f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbd518d96d6c1257e6bd",
+    "url": "/vantage-ui-2/static/js/main.7ded12b3.chunk.js"
+  },
+  {
+    "revision": "52e87ffc708e2711097b",
+    "url": "/vantage-ui-2/static/js/runtime-main.88369300.js"
+  }
+]);
